d6312fa99bef2dd97265541459aed15d205c1efe
XXuan-1 <jiaxuansun@ucsb.edu>
2023-12-10 17:03:52 -0500

    modified.

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' @title dataset
+#' @name adsl
+#' @docType data
+#' @keywords data
+#' \describe{
+#'   \item{SUBJID}{Subject ID}
+#'   \item{TRT}{Assigned Treatment}
+#'   \item{ATRT}{Actual Treatment}
+#'   \item{PRSURG}{Presence of Prior Surgery}
+#'   \item{DTHDY}{Death Day}
+#'   \item{DTH}{Death}
+#'   \item{PFSDYCR}{PFS Day (Central, RECIST)}
+#'   \item{PFSCR}{PD on Study (Central, RECIST) or Death}
+#'   \item{LIVERMET}{Metastases to Liver at Study Entry?}
+#'   \item{DIAGMONS}{Months Since Primary Diagnosis}
+#'   \item{AGE}{Age in Years at Screening}
+#'   \item{SEX}{Sex}
+#'   \item{B_WEIGHT}{Weight in Kilograms at Baseline}
+#'   \item{B_HEIGHT}{Height in Centimeters at Baseline}
+#'   \item{RACE}{Race}
+#'   \item{B_ECOG}{Baseline ECOG Performance Status}
+#'   \item{HISSUBTY}{Histological Subtype}
+#'   \item{B_METNAM}{Number of BL Metas Site}
+#'   \item{DIAGTYPE}{Primary Tumor Diagnosis}
+#' }
+NULL
